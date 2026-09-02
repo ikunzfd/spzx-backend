34517b784e6f78904d31ba9d495fdaec1f9f58d4
zfd <2111651970@qq.com>
2026-09-02 13:22:52 +0800

    搭建完后端工程

---
+++ spzx-parent/spzx-model/src/main/java/com/zfd/spzx/model/entity/product/ProductDetails.java
@@ -0,0 +1,12 @@
+package com.zfd.spzx.model.entity.product;
+
+import com.zfd.spzx.model.entity.base.BaseEntity;
+import lombok.Data;
+
+@Data
+public class ProductDetails extends BaseEntity {
+
+	private Long productId;
+	private String imageUrls;
+
+}
